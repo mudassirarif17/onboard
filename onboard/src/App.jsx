cf4d322e9ddc32c022d99c0b81f48d69abf70b7e
Mudassir <mudassirinoxent@gmail.com>
2024-08-06 23:44:25 +0500

    start working

--- onboard/src/App.jsx
+++ onboard/src/App.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1 className="bg-red-900">hello</h1>
+      <div class="bg-gradient-to-tr from-blue-200 via-blue-100 to-blue-200 h-[15vh] w-full">
+        <div>
+            <h1 className='text-center text-white'>Tuitional</h1>
+        </div>
+      </div>
     </div>
+
   )
 }
 
