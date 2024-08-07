a50ddde81ff73ac27970021c68404aef546a3695
mudassir-arif <mudassir@aptechgdn.net>
2024-08-07 14:22:48 +0500

    LIST DONE BY TH

--- onboard/src/App.jsx
+++ onboard/src/App.jsx
@@ -17,55 +17,59 @@ const App = () => {
 
         <div className="mid-top flex  items-center w-[60vw]  mx-auto">
           <div>
-            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>01</h1></div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex flex-col justify-center items-center'>
+              <h1 className='text-white font-semibold mt-8'>01</h1>
+              <h1 className='text-black font-semibold mt-3'>About</h1>
+            </div>
            
           </div>
           <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
 
           <div className=''>
-            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>02</h1></div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex flex-col justify-center items-center'>
+              <h1 className='text-white font-semibold mt-8'>02</h1>
+              <h1 className='text-black font-semibold mt-3'>Profile</h1>
+            </div>
             
           </div>
           <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
 
           <div>
-            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>03</h1></div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex flex-col justify-center items-center'>
+              <h1 className='text-white font-semibold mt-8'>03</h1>
+              <h1 className='text-black font-semibold mt-3'>Education</h1>
+            </div>
             
           </div>
           <div className='bg-[#38B6FF] w-[100px] h-[2px]'></div>
 
           <div>
-            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>04</h1></div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex flex-col justify-center items-center'>
+              <h1 className='text-white font-semibold mt-8'>04</h1>
+              <h1 className='text-black font-semibold mt-3'>Video</h1>
+            </div>
             
           </div>
           <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
 
           <div>
-            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>05</h1></div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex flex-col justify-center items-center'>
+              <h1 className='text-white font-semibold mt-8'>05</h1>
+              <h1 className='text-black font-semibold mt-3'>Availability</h1>
+            </div>
             
           </div>
           <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
 
           <div>
-            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>06</h1></div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex flex-col justify-center items-center'>
+              <h1 className='text-white font-semibold mt-8'>06</h1>
+              <h1 className='text-black font-semibold mt-3'>Subjects</h1>
+            </div>
           </div>
         </div>
 
-        <div className="mid-btm-line w-[58vw]  mx-auto">
-          <ul className='flex font-semibold space-x-[80px]'>
-            <li className='text-sm'>About</li>
-
-            <li className='text-sm '>Profile </li>
-
-            <li className='text-sm '>Education</li>
-
-            <li className='text-sm '>Video</li>
-
-            <li className='text-sm '>Availability</li>
-
-            <li className='text-sm '>Subjects</li>
-          </ul>
-        </div>
+        
 
         <div className="mid-btm w-[92vw] h-[68.2vh] mx-auto">
           <div className="mid-btm-left w-[46vw]">
