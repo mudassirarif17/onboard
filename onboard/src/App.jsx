c91e34b1b09711712f05cf64e669a8e380b35456
mudassir-arif <mudassir@aptechgdn.net>
2024-08-07 14:08:11 +0500

    Hello Commit

--- onboard/src/App.jsx
+++ onboard/src/App.jsx
@@ -1,13 +1,84 @@
-import React from 'react'
+import React from 'react';
+import logo from "./assets/logo.png";
 
 const App = () => {
   return (
     <div>
-      <div class="bg-gradient-to-tr from-blue-200 via-blue-100 to-blue-200 h-[15vh] w-full">
-        <div>
-            <h1 className='text-center text-white'>Tuitional</h1>
+
+      <div className="top-s mx-auto w-[80vw] h-[15vh] flex justify-between items-center relative">
+        <div className='bg-[#38B6FF] w-[40px] h-[40px] rounded-full'></div>
+        <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full'></div>
+        <div className='flex left-6 rounded-xl absolute top-10 w-[76vw] h-[50px] justify-center shadow-xl items-center bg-[#ffffff70]'>
+          <img className='h-[30px]' src={logo} alt="logo" />
         </div>
       </div>
+
+      <div className="mid my-4">
+
+        <div className="mid-top flex  items-center w-[60vw]  mx-auto">
+          <div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>01</h1></div>
+           
+          </div>
+          <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
+
+          <div className=''>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>02</h1></div>
+            
+          </div>
+          <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
+
+          <div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>03</h1></div>
+            
+          </div>
+          <div className='bg-[#38B6FF] w-[100px] h-[2px]'></div>
+
+          <div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>04</h1></div>
+            
+          </div>
+          <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
+
+          <div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>05</h1></div>
+            
+          </div>
+          <div className='bg-[#38B6FF] w-[100px] h-[2px] '></div>
+
+          <div>
+            <div className='bg-[#38B6FF] w-[50px] h-[50px] rounded-full flex justify-center items-center'><h1 className='text-white font-semibold'>06</h1></div>
+          </div>
+        </div>
+
+        <div className="mid-btm-line w-[58vw]  mx-auto">
+          <ul className='flex font-semibold space-x-[80px]'>
+            <li className='text-sm'>About</li>
+
+            <li className='text-sm '>Profile </li>
+
+            <li className='text-sm '>Education</li>
+
+            <li className='text-sm '>Video</li>
+
+            <li className='text-sm '>Availability</li>
+
+            <li className='text-sm '>Subjects</li>
+          </ul>
+        </div>
+
+        <div className="mid-btm w-[92vw] h-[68.2vh] mx-auto">
+          <div className="mid-btm-left w-[46vw]">
+
+          </div>
+
+          <div className="mid-btm-right w-[46vw]">
+
+          </div>
+        </div>
+
+      </div>
+
     </div>
 
   )
